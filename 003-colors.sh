4b56982caff5d547dad99b1126a09e171d5a16c0
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-03-14 19:15:05 -0400

    Bash auto push

--- 003-colors.sh
+++ 003-colors.sh
@@ -5,3 +5,10 @@
 # echo -e "\e[CoLORCODE Iam printing color \e[0m"
 
 echo -e "\e[32m Iam printing green color \e[0m"
+
+echo -e "\e[32m Iam printing green color \e[0m"
+echo -e "\e[31m Iam printing RED color \e[0m"
+
+#Syntax to use backgroud color
+
+echo -e "\e[43;32m Iam using background color and foreground color \e[0m"
