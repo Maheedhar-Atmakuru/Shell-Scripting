0423af9fc8c2fc13978dabf9e338012e9ef1b121
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-03-16 01:05:47 -0400

    Bash auto push

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -16,17 +16,17 @@ systemctl enable nginx
 echo "Starting the service"
 systemctl start nginx
 
-echo "downloading the aplication"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# echo "downloading the aplication"
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-echo "Deploying the application"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+# echo "Deploying the application"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
 
